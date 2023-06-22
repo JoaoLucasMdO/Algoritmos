ce3290f093513a0ba70e4ab09f8e8794ef6cce2d
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2023-06-22 20:05:56 -0300

    Atualizações

--- Aula16/exemplo01.py
+++
@@ -1,6 +0,0 @@
-try:
-    pypro()
-except NameError:
-    print("A função não foi definida")
-except:
-    print("Ocorreu um problema!")
--- Aula16/exemplo05.py
+++
@@ -1,8 +0,0 @@
-try:
-    num = int(input("Informe um número: "))
-except:
-    print("Valor incorreto!")
-else:
-    print(f"Você digitou {num}")
-finally:
-    print("Fim do Bloco")
